8d8b31e8df4b7cc3cabb888f3bc499b71fdf8f0a
Назар Фазылов <n.fazylov@nfazylov-nb.local>
2018-07-11 19:07:51 +0300

    initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('MERCHANT_ID', '921122');
+define('MERCHANT_PRIVATE_KEY', '125391ac36e1a6e7432ad47da64c166aa11fa91d4');
+
+define('APP_ID', '123123');
+define('CLIENT_SECRET', '5P1m3nW1Zz6vnTfd3pqo');
+
+define('SITE_APP_ID', '234234');
+define('SITE_CLIENT_SECRET', 'lUSypsLs57p1kF3Qm6Vv');
---
+++ site-example.php
@@ -0,0 +1,68 @@
+﻿<?php
+
+require_once 'config.php';
+$SITE_APP_ID = SITE_APP_ID;
+
+// берем основные данные о заказе
+$amount = 2; // стоимость
+$amount = (float)bcmul($amount, 1, 2);
+$data = [
+  'amount' => $amount,
+  'currency' => 'RUB',
+  'order_id' => 255,
+  "cashback" => [
+    "pay_time" => time(),
+    "amount_percent" => 30
+  ],
+  'ts' => time()
+];
+
+// добавляем параметры merchant_data и merchant_sign
+$merchant_data = base64_encode(json_encode($data));
+$data['merchant_data'] = $merchant_data;
+$data['merchant_sign'] = sha1($merchant_data . MERCHANT_PRIVATE_KEY);
+
+//собираем все параметры целиком в единый объект
+$params = [
+    "amount" => $amount,
+    "data" => json_encode($data),
+    "description" => "1",
+    "action" => "pay-to-service",
+    "merchant_id" => MERCHANT_ID,
+];
+
+// генерируем подпись из всех параметров кроме action, добавляем в $params
+$sign = '';
+foreach ($params as $key => $value) {
+  if ($key != 'action') {
+    $sign .= ($key.'='.$value);
+  }
+}
+$sign .= SITE_CLIENT_SECRET;
+$params['sign'] = md5($sign);
+
+// кодируем результат в JSON
+$params = json_encode($params);
+
+echo <<<HTML
+<!doctype html>
+<html>
+<head>
+  <link  rel="stylesheet" type="text/css" href="./css/style.css"/>
+  <script src="https://vk.com/js/api/openapi.js?154"></script>
+</head>
+<body>
+  <a id="pay" class="vkpay btn">Оплатить через</a>
+</body>
+<script>
+  VK.init({
+    apiId: $SITE_APP_ID
+  });
+  document.getElementById("pay").addEventListener("click", openPayform);
+
+  function openPayform() {
+    VK.App.open("vkpay", $params);
+  }
+</script>
+</html>
+HTML;
